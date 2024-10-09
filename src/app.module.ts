699a4b1f13d2f17fe0f8c1ace82a4ddd03992c27
jeanpcoelho <jeanpc0018@gmail.com>
2024-10-09 16:40:42 -0300

    app.module
    módulo principal da aplicação NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
-
+import { Module, forwardRef } from "@nestjs/common"; 
+import { AppController } from "./app.controller"; 
+import { AppService } from "./app.service"; 
+import { UserModule } from "./user/user.module"; 
+import { AuthModule } from "./auth/auth.module"; 
+import { ConfigModule } from "@nestjs/config";
+//módulo principal da aplicação NestJS
 @Module({
-  imports: [UserModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    UserModule, 
+    forwardRef(() => AuthModule), 
+    ConfigModule.forRoot({ 
+      isGlobal: true, 
+    }),
+  ],
+  //gerencia as rotas da aplicação
+  controllers: [AppController], 
+  providers: [AppService], 
 })
 export class AppModule {}
