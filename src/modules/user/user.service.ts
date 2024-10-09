89689f66f02dba8b7528b9694033ad5a6ef1beba
jeanpcoelho <jeanpc0018@gmail.com>
2024-10-09 12:44:56 -0300

    Update user.service.ts
    imports

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+    ConflictException,
+    Injectable,
+    NotFoundException,
+  } from "@nestjs/common";
+  import { UsersRepositoryService } from "./repositories/users-repository.service";
+  import { CreateUsersDTO } from "./dtos/create-usersDTO";
+  import { Users } from "@prisma/client";
+  import { UpdateUsersDTO } from "./dtos/update-usersDTO";
+  import * as bcrypt from "bcrypt";
+  
 @Injectable()
 export class UserService {}
