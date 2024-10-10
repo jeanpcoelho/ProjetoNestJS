88753868bef2020243db3e38bd5539b9f2224a7e
jeanpcoelho <jeanpc0018@gmail.com>
2024-10-10 13:42:02 -0300

    update
    correções

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,52 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
+import { AuthLoginDTO } from "./dto/auth-login.dto";
+import { AuthService } from "./auth.service";
+import { AuthRegisterDTO } from "./dto/auth-register.dto";
+import { AuthForgotDTO } from "./dto/auth-forgot.dto";
+import { AuthResetDTO } from "./dto/auth-reset.dto";
+import { AuthGuard } from "./guards/auth.guard";
+import { User } from "src/user/decorators/user.decorator";
+
+@ApiTags("auth")
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post("login")
+  @ApiResponse({ status: 201, description: "User logged in successfully." })
+  @ApiResponse({ status: 401, description: "Invalid credentials." })
+  async login(@Body() { email, password }: AuthLoginDTO) {
+    return this.authService.login(email, password);
+  }
+
+  @Post("register")
+  @ApiResponse({ status: 201, description: "User registered successfully." })
+  @ApiResponse({ status: 400, description: "Bad request." })
+  async register(@Body() body: AuthRegisterDTO) {
+    return this.authService.register(body);
+  }
+
+  @Post("forgot")
+  @ApiResponse({ status: 201, description: "Reset password link send." })
+  @ApiResponse({ status: 404, description: "User not found." })
+  async forgot(@Body() { email }: AuthForgotDTO) {
+    return this.authService.forgot(email);
+  }
+
+  @Post("reset")
+  @ApiResponse({ status: 201, description: "Password reset successfully." })
+  @ApiResponse({ status: 400, description: "Invalid or expired token." })
+  async reset(@Body() { password, token }: AuthResetDTO) {
+    return this.authService.reset(password, token);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post("me")
+  @ApiResponse({ status: 201, description: "Reset password link send." })
+  @ApiResponse({ status: 404, description: "User not found." })
+  async me(@User() user) {
+    return {
+      user,
+    };
+  }
+}
---
+++ src/auth/dto/auth-register.dto.ts
@@ -0,0 +1,58 @@
+import { ApiProperty } from "@nestjs/swagger";
+import {IsBoolean, IsEmail, IsEnum, IsOptional, IsString,  IsStrongPassword, Matches,} 
+from "class-validator";
+import { Role } from "src/user/enums/role.enum";
+
+export class AuthRegisterDTO {
+  @ApiProperty({
+    example: "dev",
+  })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    example: "dev@dev.com",
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    example: "dev123",
+  })
+  @IsStrongPassword({
+    minLength: 6,
+    minLowercase: 1,
+    minNumbers: 1,
+    minUppercase: 1,
+    minSymbols: 0,
+  })
+  password: string;
+
+  @ApiProperty({
+    example: "3399999999",
+  })
+  @Matches(/^\(?\d{2}\)?[\s-]?[\s9]?\d{4}-?\d{4}$/, {
+    message: "Invalid phone number",
+  })
+  phone: string;
+
+  @ApiProperty({
+    example: "Sao Paulo-MG",
+  })
+  @IsString()
+  city: string;
+
+  @ApiProperty({
+    example: "[1]- Para Client, [2]- Para Employee, [3]- Para Admin",
+  })
+  @IsOptional()
+  @IsEnum(Role)
+  role: number;
+
+  @ApiProperty({
+    example: "true or false",
+  })
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
