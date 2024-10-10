3ff1b6491267bd3d4ba1a0d0062a477827cd4f33
jeanpcoelho <jeanpc0018@gmail.com>
2024-10-10 14:02:06 -0300

    Update Swagger
    instalação do Swagger

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { UploadsService } from "./uploads.service";
+import { UploadsService } from "D:/Documentos/CloneGit/nestjs-prisma/src/uploads/uploads.service";
 import { UserRepositoryModule } from "src/user/repositories/users-repository.module";
 
 @Module({
