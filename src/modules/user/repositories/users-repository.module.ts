f66a51af47625ce43625db8448760f87ac90afc2
jeanpcoelho <jeanpc0018@gmail.com>
2024-10-11 12:36:43 -0300

    update

---
+++ src/modules/user/repositories/users-repository.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { UsersRepositoryService } from "./users-repository.service";
+import { PrismaModule } from "src/prisma/prisma.module";
+
+@Module({
+  imports: [PrismaModule],
+  providers: [UsersRepositoryService],
+  exports: [UsersRepositoryService],
+})
+export class UsersRepositoryModule {}
